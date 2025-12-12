179f7bc27fa9b1a241745d72323b03e5452d1000
Yeahn Kim <yeahnkim@gmail.com>
2025-12-12 04:41:25 -0800

    upgrade structure to class and add english lyrics input

--- lyrics.py
+++ lyrics.py
@@ -1,177 +1,205 @@
 import tkinter as tk
 from tkinter import messagebox, filedialog, font, ttk
-# from tkinter import filedialog
 from pptx import Presentation
 from pptx.util import Inches, Pt, Cm
 from pptx.enum.text import PP_ALIGN, MSO_ANCHOR
 from pptx.dml.color import RGBColor
 
-def create_presentation():
-    # get text input
-    text_input = text_entry.get("1.0", tk.END).strip()
-    lines = text_input.splitlines()
-    text_input = []
-    for l in lines:
-        if not l == "":
-            text_input.append(l)
+class Lyrics(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("가사 파워포인트 생성 프로그램")
 
-    if not text_input:
-        messagebox.showwarning("Input Error", "Please enter text for the slide.")
-        return
+        self.ppt = Presentation()
+
+        self.entry_widgets = {}
+        # Contains entries named: [lyrics, english lyrics, font size, font family]
+
+        self.create_frame()
+
+        self.after(0, self.center_window)
     
-    # get font size
-    while True:
-        try:
-            font_size = int(font_entry.get())
-            break
-        except ValueError:
-            messagebox.showwarning("Input Error", "Font size has to be an integer.")
-            return
-    
-    # # get file name
-    # file_name = name_entry.get()
-    # if not file_name:
-    #     messagebox.showwarning("Input Error", "Please give a file name.")
-    #     return
+    def create_frame(self):
+        # Lyrics Frame
+        self.lyrics_frame = tk.Frame(self)
+        self.lyrics_frame.grid(row=0, column=0, columnspan=3, padx=10, pady=10, sticky="nsew")
 
-    # get font family
-    font_family = font_family_combobox.get()
+        tk.Label(self.lyrics_frame, text="가사 입력").pack()
+        lyrics_text = tk.Text(self.lyrics_frame, width=50, height=20)
+        lyrics_text.pack(pady=5)
+        self.entry_widgets["lyrics"] = lyrics_text
 
-    # textbox width
-    text_box_width = ppt.slide_width.inches
-    # Approximate width per character (this is a rough estimate)
-    avg_char_width = font_size * 0.75  # Average width of characters (in points)
-    # Convert text box width from inches to points (1 inch = 72 points)
-    text_box_width_in_points = text_box_width * 72
-    # Estimate the number of characters per line (for korean)
-    chars_per_line = int(text_box_width_in_points / avg_char_width)
+        # Input Frame
+        self.input_frame = tk.Frame(self)
+        self.input_frame.grid(row=1, column=0, columnspan=3, padx=10, pady=10, sticky="nsew")
+        self.input_frame.grid_columnconfigure(1, weight=1)
+        self.input_frame.grid_columnconfigure(2, weight=1)
+        input_row_n = 0
 
-    # for idx in range(0, len(text_input), 1):
-    idx = 0
-    while(idx < len(text_input)):
-        text = text_input[idx]
-        if idx+1 >= len(text_input):
-            break
- 
-        # print("len(text):", len(text))
-        # print("chars per line:", chars_per_line)
-        # text doesn't overflow & next line doesn't overflow
-        if len(text) < chars_per_line and len(text_input[idx+1]) < chars_per_line:
-           text += "\n" + text_input[idx+1]
-           idx+=1
-        print(text)
-        create_slide(text, font_size, font_family)
-        idx+=1
-    
-    save_path = filedialog.asksaveasfilename(
-        defaultextension=".pptx",
-        filetypes=[("PowerPoint files", "*.pptx")],
-        title="Save Presentation As"
-    )
+        ## font size entry
+        font_default = 48
+        tk.Label(self.input_frame, text="Font Size:", anchor="e").grid(row=input_row_n, column=0, padx=0, pady=5, sticky="nsew")
+        font_entry = tk.Entry(self.input_frame)
+        font_entry.insert(0, str(font_default))
+        font_entry.grid(row=input_row_n, column=1, columnspan=2, padx=0, pady=5, sticky="nsew")
 
-    if not save_path:
-        return
+        self.entry_widgets["font size"] = font_entry
+        input_row_n+=1
+
+        ## font family entry
+        # Dynamically get the list of fonts from the system
+        font_families = sorted(list(font.families()))
+
+        tk.Label(self.input_frame, text="Font:", anchor="e").grid(row=input_row_n, column=0, padx=0, pady=5, sticky="nsew")
+        font_family_combobox = ttk.Combobox(self.input_frame, values=font_families, state="readonly")
+        font_family_combobox.grid(row=input_row_n, column=1, columnspan=2, padx=0, pady=5, sticky="nsew")
+        # Check if a common font is in the list to set as a default
+        if "Arial" in font_families:
+            font_family_combobox.set("Arial")
+        else:
+            font_family_combobox.set(font_families[0])
+
+        self.entry_widgets["font family"] = font_family_combobox
+        input_row_n+=1
+
+        ## Number of lines per slide
+        tk.Label(self.input_frame, text="# of lines per slide:", anchor="e").grid(row=input_row_n, column=0, padx=0, pady=5, sticky="nsew")
+        line_num_combobox = ttk.Combobox(self.input_frame, textvariable="2", values=[str(y) for y in range(1, 5)], state="readonly")
+        line_num_combobox.set(2)
+        line_num_combobox.grid(row=input_row_n, column=1, columnspan=2, padx=0, pady=5, sticky="nsew")
+
+        self.entry_widgets["num of lines"] = line_num_combobox
+        input_row_n+=1
+
+        ## Line position
+        tk.Label(self.input_frame, text="English lyrics?", anchor="e").grid(row=input_row_n, column=0, padx=0, pady=5, sticky="nsew")
+        self.yn_var = tk.BooleanVar(value=False)
+        yes_btn = tk.Radiobutton(self.input_frame, text="Yes", anchor="w", variable=self.yn_var, value=True, command=self.english_select)
+        yes_btn.grid(row=3, column=1, padx=0, pady=5, sticky="nsew")
+        no_btn = tk.Radiobutton(self.input_frame, text="No", anchor="w", variable=self.yn_var, value=False, command=self.english_select)
+        no_btn.grid(row=3, column=2, padx=0, pady=5, sticky="nsew")
+
+        # English Lyrics Frame
+        self.english_lyrics_frame = tk.Frame(self)
+        self.english_lyrics_frame.grid(row=0, column=3, columnspan=3, padx=10, pady=10, sticky="nsew")
+
+        tk.Label(self.english_lyrics_frame, text="영어 가사 입력").pack()
+        eng_lyrics = tk.Text(self.english_lyrics_frame, width=50, height=20)
+        eng_lyrics.pack(pady=5)
+        self.entry_widgets["english lyrics"] = eng_lyrics
+
+        self.english_lyrics_frame.grid_forget()
         
-    # ppt.save('./'+file_name+'.pptx')
-    ppt.save(save_path)
-    gui.destroy()
 
-def create_slide(text, font_size, font_family):
-    slide_layout = ppt.slide_layouts[6]
-    slide = ppt.slides.add_slide(slide_layout)
+        # Create presentation button
+        create_btn = tk.Button(self, text="파워포인트 생성", command=self.create_presentation)
+        create_btn.grid(row=3, columnspan=6, pady=20)
 
-    # slide dimensions
-    slide_width = ppt.slide_width.inches
-    slide_height = ppt.slide_height.inches
-
-    # text box dimensions
-    # text_width = Inches(slide_width)
-    text_height = Cm(4.37)
+    def english_select(self):
+        if self.yn_var.get():
+            self.english_lyrics_frame.grid(row=0, column=3, columnspan=3, padx=10, pady=10, sticky="nsew")
+        else:
+            self.english_lyrics_frame.grid_forget()
     
-    # slide background
-    background = slide.background
-    fill = background.fill
-    fill.solid()
-    fill.fore_color.rgb = RGBColor(0,0,0)
+    def create_presentation(self):
+        # get text input
+        lyrics_text = self.entry_widgets["lyrics"].get("1.0", tk.END).strip()
+        lines = lyrics_text.splitlines()
+        lyrics_text = []
+        for l in lines:
+            if not l == "":
+                lyrics_text.append(l)
 
-    # centering text box
-    left = (slide_width - text_width.inches) / 2
-    top = (slide_height - text_height.inches) / 2
+        if not lyrics_text:
+            messagebox.showwarning("Input Error", "Please enter text for the slide.")
+            return
+        
+        # get font size
+        while True:
+            try:
+                font_size = int(self.entry_widgets["font size"].get().strip())
+                break
+            except ValueError:
+                messagebox.showwarning("Input Error", "Font size has to be an integer.")
+                return
+        
+        # get font family
+        font_family = self.entry_widgets["font family"].get().strip()
 
-    textBox = slide.shapes.add_textbox(Inches(left), Inches(top), text_width, text_height)
-    text_frame = textBox.text_frame
-    # center vertically
-    text_frame.vertical_anchor = MSO_ANCHOR.MIDDLE
+        # for idx in range(0, len(lyrics_text), 1):
+        idx = 0
+        while(idx < len(lyrics_text)):
+            text = lyrics_text[idx]
+            if idx+1 >= len(lyrics_text):
+                self.create_slide(text, font_size, font_family)
+                break
+            text += "\n" + lyrics_text[idx+1]
+            idx+=2
+            self.create_slide(text, font_size, font_family)
+        
+        save_path = filedialog.asksaveasfilename(
+            defaultextension=".pptx",
+            filetypes=[("PowerPoint files", "*.pptx")],
+            title="Save Presentation As"
+        )
 
-    # Style the text
-    paragraph = text_frame.paragraphs[0]
-    paragraph.alignment = PP_ALIGN.CENTER
-    run = paragraph.add_run()
-    run.text = text
-    run.font.name = font_family
-    run.font.size = Pt(font_size)
-    run.font.color.rgb = RGBColor(255, 255, 255)
+        if not save_path:
+            return
+            
+        # ppt.save('./'+file_name+'.pptx')
+        self.ppt.save(save_path)
+        self.destroy()
 
+    def create_slide(self, text, font_size, font_family):
+        slide_layout = self.ppt.slide_layouts[6]
+        slide = self.ppt.slides.add_slide(slide_layout)
 
-ppt = Presentation()
+        # slide dimensions
+        slide_width = self.ppt.slide_width.inches
+        slide_height = self.ppt.slide_height.inches
 
-# Set the slide size (example: 16:9 widescreen using Inches)
-ppt.slide_width = Inches(13.33)  # Width
-ppt.slide_height = Inches(7.5)  # Height
+        # text box dimensions
+        text_width = Inches(slide_width)
+        text_height = Cm(4.37)
+        
+        # slide background
+        background = slide.background
+        fill = background.fill
+        fill.solid()
+        fill.fore_color.rgb = RGBColor(0,0,0)
 
-text_width = Inches(ppt.slide_width.inches)
+        # centering text box
+        left = (slide_width - text_width.inches) / 2
+        top = (slide_height - text_height.inches) / 2
 
-gui = tk.Tk()
-gui.title("Slide Maker")
+        textBox = slide.shapes.add_textbox(Inches(left), Inches(top), text_width, text_height)
+        text_frame = textBox.text_frame
+        # center vertically
+        text_frame.vertical_anchor = MSO_ANCHOR.MIDDLE
 
-# Lyrics entry
-lyrics_frame = tk.Frame(gui)
-lyrics_frame.grid(row=0, column=0, columnspan=3, padx=10, pady=10, sticky="w")
+        # Style the text
+        paragraph = text_frame.paragraphs[0]
+        paragraph.alignment = PP_ALIGN.CENTER
+        run = paragraph.add_run()
+        run.text = text
+        run.font.name = font_family
+        run.font.size = Pt(font_size)
+        run.font.color.rgb = RGBColor(255, 255, 255)
 
-tk.Label(lyrics_frame, text="Enter Lyrics").pack()
-text_entry = tk.Text(lyrics_frame, width=50, height=20)
-text_entry.pack(pady=5)
+    def center_window(self):
+        self.update_idletasks()  # Ensure widgets are rendered
 
-# font size
-font_size_frame = tk.Frame(gui)
-font_size_frame.grid(row=1, column=0, columnspan=3, padx=10, pady=5, sticky="w")
+        window_width = self.winfo_width()
+        window_height = self.winfo_height()
 
-font_default = 48
-tk.Label(font_size_frame, text="Font Size:").pack(side="left")
+        screen_width = self.winfo_screenwidth()
+        screen_height = self.winfo_screenheight()
 
-font_entry = tk.Entry(font_size_frame, width=5)
-font_entry.insert(0, str(font_default))
-font_entry.pack(side="left", padx=5)
+        center_x = (screen_width - window_width) // 2
+        center_y = (screen_height - window_height) // 2
 
-# font family selection
-font_family_frame = tk.Frame(gui)
-font_family_frame.grid(row=2, column=0, columnspan=3, padx=10, pady=5, sticky="w")
-
-tk.Label(font_family_frame, text="Font:").pack(side="left")
-
-# Dynamically get the list of fonts from the system
-font_families = sorted(list(font.families()))
-
-font_family_combobox = ttk.Combobox(font_family_frame, values=font_families, state="readonly")
-# Check if a common font is in the list to set as a default
-if "Arial" in font_families:
-    font_family_combobox.set("Arial")
-else:
-    font_family_combobox.set(font_families[0])
-
-font_family_combobox.pack(side="left", padx=5)
-
-# # file name
-# name_frame = tk.Frame(gui)
-# name_frame.grid(row=2, column=0, columnspan=3, padx=10, pady=5, sticky="w")
-# tk.Label(name_frame, text="File Name:").pack(side="left")
-
-# name_entry = tk.Entry(name_frame, width=20)
-# name_entry.pack(side="left", padx=0)
-
-# tk.Label(name_frame, text=".pptx").pack(side="left", padx=0)
-
-# Create presentation button
-tk.Button(gui, text="Create Presentation", command=create_presentation).grid(row=3, columnspan=3, pady=20)
-
-gui.mainloop()
+        self.geometry(f"+{center_x}+{center_y}")
+    
+if __name__ == "__main__":
+    app = Lyrics()
+    app.mainloop()
